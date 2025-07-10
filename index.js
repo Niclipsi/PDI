3a33b562b32fb90efe9e89f44cd1fbc809e52667
nicolás muñoz <nm335791@gmail.com>
2025-07-10 00:09:43 -0300

    nueva rama PDI

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("somos (alumnos( y este es el trabajo de node.js para PDI (la mejor materia)")
+import chalk from 'chalk';
+import { format } from 'date-fns';
+
+console.log(chalk.green("Hola, soy Nicolás” en color verde"))
+
+console.log(chalk.green("Hola, soy Dylan” en color verde"))
+
+console.log(chalk.green("Hola, soy Hugo en color verde"))
+
+console.log(chalk.green("Hola, soy Félipe en color verde"))
+
+const hoy = new Date();
+const fechaFormateada = format(hoy, 'dd/MM/yyyy');
+console.log(chalk.blue(fechaFormateada));
+
+const entrega = new Date(hoy.getFullYear(), 5, 5); 
+
+if (hoy < entrega) {
+  console.log(chalk.yellow(`Fecha de entrega: ${fechaFormateada}`));
+} else {
+  console.log(chalk.red(`Fecha de entrega: ${fechaFormateada}`));
+}
